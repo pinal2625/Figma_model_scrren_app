95ce6a9532b38e60e57b1309d18f8af210a4f322
Pinal trapasiya <pinaltrapasiya2213@gmail.com>
2023-04-20 15:10:11 +0530

    first commit

---
+++ lib/res/constant/image.dart
@@ -0,0 +1,16 @@
+class SplashImage {
+  static const String imagePath = "assets/images/";
+
+  // Images ------>>
+  static const String appImage = "${imagePath}gonouts.png";
+}
+
+class ModelImage {
+  static const String imagePath = "assets/images/";
+  static const String Image = "${imagePath}Logo.png";
+  static const String ege = "${imagePath}ege.png";
+  static const String star = "${imagePath}Star 4.png";
+  static const String Wine = "${imagePath}Wine image.png";
+  static const String Wine_3 = "${imagePath}Wine image_2.png";
+  static const String Wine_2 = "${imagePath}Wine image_1.png";
+}
